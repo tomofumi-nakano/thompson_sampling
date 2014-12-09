571ec146278fbaccf1cb95f824d37f9373e0dbf8
Tomofumi NAKANO <Tomofumi_Nakano@voyagegroup.com>
2014-12-09 12:01:35 +0900

    price から cpc に変更

--- thompson.rb
+++ thompson.rb
@@ -4,40 +4,22 @@
 require 'rbeta.rb'
 
 # thompson sampling
-# @param [Array] arms [[imps, click, price]]
+# @param [Array] arms [imps, click, cpc]
 # @param [Fixnum] loop 繰り返し回数
-# @return [Array] ratio_list 配信比率 [[ratio]]
+# @return [Array] ratio_list 配信比率 [ratio]
 def thompson(arms, loop = 1000)
-  # cpc の決定と rbeta 初期化
-  cpcs = []
+  # rbeta 初期化
   rbs = []
-  max_cpc = 0 # クリックが0のときのため
-  arms.each.with_index  do |(imps, click, price), id|
-    if (click > 0)
-      cpcs[id] = price.to_f/click
-      if (max_cpc < cpcs[id])
-        max_cpc = cpcs[id]
-      end
-    else
-      cpcs[id] = -1 # 一旦-1に指定
-    end
+  arms.each.with_index  do |(imps, click, _), id|
     rbs[id] = RBetaQ.new(click+1,imps-click+1) # +1 でいく
   end # each
-  if (max_cpc == 0)  # 全てがクリック0のときは1に設定する
-    max_cpc = 1.0  # 最終的な出力結果は配信比率なので1で問題ない
-  end
-  arms.each.with_index do |data, id| # クリックが0のとき、最大のCPCに設定する
-    if (cpcs[id] < 0)
-      cpcs[id] = max_cpc
-    end
-  end
 
   # thompson sampling
-  ratio = Array.new(cpcs.size,0)
+  ratio = Array.new(arms.size,0)
   for i in 0...loop
     max = -1
     max_id = -1
-    cpcs.each.with_index do |cpc,id|
+    arms.each.with_index do |(_, _, cpc),id|
       v = cpc * rbs[id].rand
       if (max < v)
         max = v
